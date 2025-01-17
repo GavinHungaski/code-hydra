a87be8c016997cd2c8692ba654e755b56a9dd009
GavinHungaski <ghungaski@gmail.com>
2025-01-17 15:28:40 -0500

    added chat page text areas

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -4,10 +4,25 @@ export default function ChatPage() {
     position: 'relative',
     width: '100vw',
   }
+  const text_inputStyle = {
+    width: '90vw',
+    height: '5vh',
+    resize: 'none',
+    marginTop: '20px',
+  }
+  const chat_outputStyle = {
+    width: '90vw',
+    height: '30vh',
+    resize: 'none',
+    overflow: 'auto',
+    marginTop: '20px',
+  }
 
   return (
     <div style={divStyle}>
       <h1>Chat Page</h1>
+      <textarea style={text_inputStyle} placeholder="Question"></textarea>
+      <textarea style={chat_outputStyle} placeholder="Response"></textarea>
     </div>
   )
 }
