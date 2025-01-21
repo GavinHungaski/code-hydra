3a00ad67a2fcdd73bc6e08c923eb0b3f38bdb41d
GavinHungaski <ghungaski@gmail.com>
2025-01-21 11:33:52 -0500

    small cosmetic changes

--- src/components/ModelPicker.jsx
+++ src/components/ModelPicker.jsx
@@ -1,24 +1,32 @@
 
 const containerStyle = {
-    border: '1px solid #ADD8E6',
+    border: '2px solid #36454F',
     color: 'black',
     borderRadius: '45px',
     width: '30vw',
     margin:'0px auto 20px auto',
     backgroundColor: '#C0C0C0',
 }
-
 const selectStyle = {
-    marginBottom: '20px',
+    margin: '10px auto 20px auto',
     height: '25%',
     width: '70%',
     borderRadius: '15px',
+}
+const h2Style = {
+    margin: '10px auto 10px auto',
+}
+const hrStyle = {
+    width: '90%',
+    margin: '0 auto',
+    color: 'black',
 }
 
 export default function ModelPicker() {
     return (
         <div style={containerStyle}>
-            <h2>Model:</h2>
+            <h2 style={h2Style}>Model:</h2>
+            <hr style={hrStyle} />
             <select style={selectStyle}>
                 <option value="qwen-coder">Qwen/Qwen2.5-Coder-32B-Instruct</option>
             </select>
