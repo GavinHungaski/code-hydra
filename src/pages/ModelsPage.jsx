0ac5b653b309f33dbb9059f91e7b331e1a41beb4
GavinHungaski <ghungaski@gmail.com>
2025-01-22 17:50:24 -0500

    model card changes

--- src/pages/ModelsPage.jsx
+++ src/pages/ModelsPage.jsx
@@ -21,24 +21,6 @@ export default function ModelsPage() {
                 description={'Qwen2.5-Coder is the latest series of Code-Specific Qwen large language models (formerly known as CodeQwen). As of now, Qwen2.5-Coder has covered six mainstream model sizes, 0.5, 1.5, 3, 7, 14, 32 billion parameters, to meet the needs of different developers.'}
                 link={'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'}
             />
-            <ModelCard 
-                img={'/qwen-icon.png'}
-                title={'Qwen2.5-Coder-32B-Instruct'}
-                description={'Qwen2.5-Coder is the latest series of Code-Specific Qwen large language models (formerly known as CodeQwen). As of now, Qwen2.5-Coder has covered six mainstream model sizes, 0.5, 1.5, 3, 7, 14, 32 billion parameters, to meet the needs of different developers.'}
-                link={'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'}
-            />
-            <ModelCard 
-                img={'/qwen-icon.png'}
-                title={'Qwen2.5-Coder-32B-Instruct'}
-                description={'Qwen2.5-Coder is the latest series of Code-Specific Qwen large language models (formerly known as CodeQwen). As of now, Qwen2.5-Coder has covered six mainstream model sizes, 0.5, 1.5, 3, 7, 14, 32 billion parameters, to meet the needs of different developers.'}
-                link={'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'}
-            />
-            <ModelCard 
-                img={'/qwen-icon.png'}
-                title={'Qwen2.5-Coder-32B-Instruct'}
-                description={'Qwen2.5-Coder is the latest series of Code-Specific Qwen large language models (formerly known as CodeQwen). As of now, Qwen2.5-Coder has covered six mainstream model sizes, 0.5, 1.5, 3, 7, 14, 32 billion parameters, to meet the needs of different developers.'}
-                link={'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'}
-            />
         </div>
     )
 }
