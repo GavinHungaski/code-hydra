d7c17dd9561163c8e4ed36124b8c82a8748e20b2
GavinHungaski <ghungaski@gmail.com>
2025-01-22 17:54:25 -0500

    model card changes

--- src/pages/ModelsPage.jsx
+++ src/pages/ModelsPage.jsx
@@ -21,6 +21,12 @@ export default function ModelsPage() {
                 description={'Qwen2.5-Coder is the latest series of Code-Specific Qwen large language models (formerly known as CodeQwen). As of now, Qwen2.5-Coder has covered six mainstream model sizes, 0.5, 1.5, 3, 7, 14, 32 billion parameters, to meet the needs of different developers.'}
                 link={'https://huggingface.co/Qwen/Qwen2.5-Coder-32B-Instruct'}
             />
+            <ModelCard 
+                img={'/deepseek-icon.png'}
+                title={'DeepSeek-Coder-V2-Instruct'}
+                description={'We present DeepSeek-Coder-V2, an open-source Mixture-of-Experts (MoE) code language model that achieves performance comparable to GPT4-Turbo in code-specific tasks. Specifically, DeepSeek-Coder-V2 is further pre-trained from an intermediate checkpoint of DeepSeek-V2 with additional 6 trillion tokens.'}
+                link={'https://huggingface.co/deepseek-ai/DeepSeek-Coder-V2-Instruct'}
+            />
         </div>
     )
 }
